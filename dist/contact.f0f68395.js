e8ca587ef5dc0e54a13ea3a900dde10bf44ed3f1
Lav Kushwaha <lavkumar1224@gmail.com>
2023-12-18 18:47:37 +0530

    Food Ordering Website

--- dist/contact.f0f68395.js
+++
@@ -1,2 +0,0 @@
-var e=globalThis.parcelRequire141a;(0,e.register)("7ZnbQ",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),i=()=>(0,a.jsx)("div",{children:(0,a.jsx)("h2",{children:"Contact"})})});
-//# sourceMappingURL=contact.f0f68395.js.map
